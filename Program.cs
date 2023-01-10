6d7b48cfdde67d81f7d487418a6b98dc7841e565
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-10 14:13:11 +0530

    [chiru] Creating Contact

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBook Problem");
+            Person person = new Person();
+            person.AddPerson();
+        }
+    }
+}
