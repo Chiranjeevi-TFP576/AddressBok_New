1784dca3891bea764ad09fea6a9f272891b9728c
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-11 20:27:04 +0530

    [chiru] Display Contact Details

---
+++ AddressBook_New/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_New
+{
+    public class Person
+    {
+        public List<Contact> personDetails = new List<Contact>();
+        //UC1_Create Contact
+        public void AddPerson()
+        {
+            Contact person = new Contact();
+
+            Console.WriteLine("Enter first name");
+            person.firstName = Console.ReadLine();
+            Console.WriteLine("Enter last name");
+            person.lastName = Console.ReadLine();
+            Console.WriteLine("Enter address name");
+            person.address = Console.ReadLine();
+            Console.WriteLine("Enter city name");
+            person.city = Console.ReadLine();
+            Console.WriteLine("Enter state name");
+            person.state = Console.ReadLine();
+            Console.WriteLine("Enter zip name");
+            person.zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            person.phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email id");
+            person.email = Console.ReadLine();
+            personDetails.Add(person);
+        }
+        //Adding another contact
+        public void MultiPersons()
+        {
+            AddPerson();
+            Console.Write("\n Do You Want To Add Another Contact (Y/N) :  ");
+            string choice = Console.ReadLine();
+            while (choice == "Y" || choice == "y")
+            {
+                AddPerson();
+                Console.Write("\nDo You Want To Add Another Contact (Y/N) :  ");
+                choice = Console.ReadLine();
+            }
+
+        }
+        //Printing the address book details 
+        public void Display()
+        {
+            foreach (Contact person in personDetails)
+            {
+                Console.WriteLine("---Address book details----");
+                Console.WriteLine("First Name:" + person.firstName);
+                Console.WriteLine("Last Name:" + person.lastName);
+                Console.WriteLine("Address:" + person.address);
+                Console.WriteLine("City:" + person.city);
+                Console.WriteLine("State:" + person.state);
+                Console.WriteLine("Zip:" + person.zip);
+                Console.WriteLine("Phone Number:" + person.phoneNumber);
+                Console.WriteLine("Email:" + person.email);
+            }
+        }
+    }
+}
---
+++ AddressBook_New/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AddressBook_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBook Problem");
+
+            Person person = new Person();
+            while(true)
+            {
+                Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-View the AddressBook ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        person.AddPerson();
+                        Console.WriteLine("Enter any key to exit");
+                        break;
+                    case 2:
+                        person.Display();
+                        Console.WriteLine("Enter any key to exit");
+                        break;
+                }
+            }
+           
+                   
+        }
+    }
+}
