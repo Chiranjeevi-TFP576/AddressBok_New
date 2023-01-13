b7f1cba51feac8133b612123e23b7a0f770e03af
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-13 21:58:24 +0530

    [chiru] EditingContactDetails

--- AddressBook_New/Program.cs
+++ AddressBook_New/Program.cs
@@ -9,7 +9,7 @@
             Person person = new Person();
             while(true)
             {
-                Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-View the AddressBook ");
+                Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-View the AddressBook \n 3-Edit the Address Book  ");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -19,6 +19,10 @@
                         break;
                     case 2:
                         person.Display();
+                        Console.WriteLine("Enter any key to exit");
+                        break;
+                    case 3:
+                        person.EditContact();
                         Console.WriteLine("Enter any key to exit");
                         break;
                 }
